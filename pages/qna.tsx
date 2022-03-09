cd111012bfdd4d0e4dd90646321726d56bc4cc82
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-03-09 09:23:57 +0900

    fix questionbox to component

--- pages/qna.tsx
+++ pages/qna.tsx
@@ -1,12 +1,7 @@
 import { NextPage } from "next";
-import Image from "next/image";
-
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 import styles from "../styles/QuestionAnswerPage.module.scss";
-import LoremIpsum from "react-lorem-ipsum";
+import QuestionBoxComponent from "../components/QuestionBoxComponent";
 
 interface Props {}
 
@@ -14,26 +9,7 @@ const QuestionAnswerPage: NextPage<Props> = () => {
   return (
       <div className={styles.container}>
           <div className={styles.mainContainer}>
-              <div className={styles.questionBox}>
-                  <div className={styles.questionTitle}>
-                      <QuestionMarkIcon className={styles.questionMarkIcon} />
-                      <div>제목을 입력하세요</div>
-                  </div>
-                  <div className={styles.questionText}>
-                      <LoremIpsum p={2} />
-                  </div>
-                  <div className={styles.questionBottom}>
-                      <div className={styles.questionInfo}>
-                          <AccountCircleIcon className={styles.accountCircleIcon} />
-                          <div className={styles.questionUser}>user 님의 질문</div>
-                          <div className={styles.questionTime}>2022.02.28</div>
-                      </div>
-                      <div className={styles.questionComment}>
-                          <ChatBubbleIcon className={styles.chatBubbleIcon} />
-                          <div>댓글쓰기</div>
-                      </div>
-                  </div>
-              </div>
+              <QuestionBoxComponent />
           </div>
       </div>
   );
