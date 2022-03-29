dc9ad9c4df601f02905c37a4c06bea1ffe864703
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-03-29 23:58:32 +0900

    feat ClassPostPreview

--- pages/agora/[class_name]/index.tsx
+++ pages/agora/[class_name]/index.tsx
@@ -1,22 +1,30 @@
 import { GetServerSideProps, NextPage } from "next";
 import { queryToString } from "../../../utility";
 
+import styles from "../../../styles/AgoraListPage.module.scss";
+import ClassPostPreview from "../../../components/Reused/ClassPostPreview";
+
 interface Props {
   className: string | null;
 }
 
 const AgoraListPage: NextPage<Props> = ({ className }) => {
-  return <div>class name: [{className}]</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.className}>강의 제목</div>
+      {[...Array(10)].map((item) => {
+        return <ClassPostPreview key={item} />;
+      })}
+    </div>
+  );
 };
 
 export default AgoraListPage;
 
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context
-) => {
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
   return {
     props: {
-      className: queryToString(context.params?.class_name),
-    },
+      className: queryToString(context.params?.class_name)
+    }
   };
 };
