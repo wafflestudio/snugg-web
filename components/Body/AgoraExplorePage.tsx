74a2ad4587aacd35026ecf8326e38c214f20e769
ChrisLee02 <hdchris765@snu.ac.kr>
2022-04-02 20:53:18 +0900

    리뷰 수정

--- components/Body/AgoraExplorePage.tsx
+++ components/Body/AgoraExplorePage.tsx
@@ -3,17 +3,35 @@ import styles from "/styles/AgoraExplorePage.module.scss";
 import LectureInfo from "../Reused/LectureInfo";
 
 const AgoraExplorePage = () => {
+  const lectureInfo = {
+    courseName: "대학영어",
+    professor: "홍길동",
+    credits: 3,
+    courseCode: "L0441.000600 / 007",
+    studentNumber: 22,
+    grade: 3,
+    dept: "영어영문학과",
+    key: 1,
+  };
+  const lectureInfos = [];
+  for (let i = 0; i < 10; i++) {
+    lectureInfos.push({ ...lectureInfo, key: i });
+  }
   return (
     <div className={styles.page}>
       <div className={styles.text}>인기 강의</div>
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
-      <LectureInfo />
+      {lectureInfos.map((lectureInfo) => (
+        <LectureInfo
+          courseCode={lectureInfo.courseCode}
+          courseName={lectureInfo.courseName}
+          credits={lectureInfo.credits}
+          dept={lectureInfo.dept}
+          grade={lectureInfo.grade}
+          professor={lectureInfo.professor}
+          studentNumber={lectureInfo.studentNumber}
+          key={lectureInfo.key}
+        />
+      ))}
     </div>
   );
 };
--- components/Reused/LectureInfo.tsx
+++ components/Reused/LectureInfo.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import styles from "/styles/LectureInfo.module.scss";
 import { Person } from "@mui/icons-material";
-const LectureInfo = () => {
+
+interface Props {
+  courseName: string;
+  professor: string;
+  credits: number;
+  courseCode: string;
+  studentNumber: number;
+  grade: number;
+  dept: string;
+}
+
+const LectureInfo = (props: Props) => {
   return (
     <div className={styles.lectureInfo}>
       <div className={styles.top}>
         <div className={styles.topLeft}>
-          <span className={styles.boldText}>대학영어</span>
-          <span className={styles.regularText}>박여선/3학점</span>
+          <span className={styles.boldText}>{props.courseName}</span>
+          <span className={styles.regularText}>
+            {props.professor}/{props.credits}
+          </span>
         </div>
         <div className={styles.topRight}>
-          <span className={styles.regularText}>L0441.000600 / 007</span>
+          <span className={styles.regularText}>{props.courseCode}</span>
           <Person sx={{ color: "#C4C4C4" }} />
-          <span className={styles.greyText}>22</span>
+          <span className={styles.greyText}>{props.studentNumber}</span>
         </div>
       </div>
 
       <div className={styles.bottom}>
-        <span>영어영문학과, 1학년</span>
+        <span>
+          {props.dept}, {props.grade}학년
+        </span>
       </div>
     </div>
   );
