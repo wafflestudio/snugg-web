176a59bed7fe525bb3276c747aeb1d034bfd2b0b
joongwon <winterdawnlight@snu.ac.kr>
2022-04-13 01:22:27 +0900

    add listQna api

--- api/index.ts
+++ api/index.ts
@@ -14,6 +14,18 @@ export interface User {
   last_login?: string;
 }
 
+export interface Post {
+  pk: number;
+  field: string;
+  writer?: User;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at?: string;
+  accepted_answer?: number;
+  tags: string[];
+}
+
 export interface UserTokenResponse {
   user: User;
   token: {
@@ -22,8 +34,14 @@ export interface UserTokenResponse {
   };
 }
 
-interface SuccessResponse {
+export interface SuccessResponse {
   success: true;
+}
+
+export interface ListQnaResponse {
+  next: string;
+  prev: string;
+  results: Post[];
 }
 
 export interface SignInParams {
@@ -42,6 +60,19 @@ export interface SignUpParams {
   birth_date: string | null;
 }
 
+export interface ListQnaParams {
+  /** 페이지네이션 관련 */
+  cursor?: string;
+  ordering?: string;
+  page_size?: number;
+
+  /** 필터링 관련 */
+  field?: string;
+  tag?: string;
+  writer?: number;
+}
+
+
 const api = {
   signIn: async (params: SignInParams) =>
     await axios.post<UserTokenResponse>("/auth/signin/", params),
@@ -49,6 +80,8 @@ const api = {
     await axios.post<SuccessResponse>("/auth/signout/", params),
   signUp: async (params: SignUpParams) =>
     await axios.post<UserTokenResponse>("/auth/signup/", params),
+  listQna: async (params: ListQnaParams) =>
+    await axios.get<ListQnaResponse>("/qna/posts", {params}),
 };
 
 export default api;
