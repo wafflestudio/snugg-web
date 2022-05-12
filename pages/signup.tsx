46f335c167f328c6d52ef60b0d638d953fabe2cd
이하동 <hdchris765@snu.ac.kr>
2022-05-12 15:56:32 +0900

    Revert "Revert "Feat/connect page""

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,12 +3,14 @@ import { NextPage } from "next";
 import { SignUpPage } from "../components/pages/root/SignUpPage";
 import { useAppDispatch } from "../store";
 import { signUp } from "../store/users";
+import { useRouter } from "next/router";
 
 interface Props {}
 
 const SignUpPageContainer: NextPage<Props> = () => {
   const validateId = async (_id: string) => true;
   const dispatch = useAppDispatch();
+  const router = useRouter();
   const onFormSubmit = (
     email: string,
     id: string,
@@ -19,6 +21,7 @@ const SignUpPageContainer: NextPage<Props> = () => {
       .then((action) => {
         if (signUp.fulfilled.match(action)) {
           alert(`sign up success! ${action.payload.user.username}`);
+          router.push("/question");
         } else if (signUp.rejected.match(action)) {
           alert(`sign in failure! ${JSON.stringify(action.error)}`);
         }
