765b50931378a97300ebcd3e9da3d4a6889257d3
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-05-21 21:15:53 +0900

    fix error from merging

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_ENDPOINT = "https://fp026w45m5.execute-api.ap-northeast-2.amazonaws.com/";
+export const API_ENDPOINT =
+  "https://fp026w45m5.execute-api.ap-northeast-2.amazonaws.com/";
 
 const isProduction = process.env.NODE_ENV === "production";
 const isServer = typeof window === "undefined";
@@ -94,7 +95,7 @@ export interface PostParams {
 }
 
 export interface AnswerPost {
-  pk: number;
+  pk?: number;
   post: number;
   content: string;
 }
