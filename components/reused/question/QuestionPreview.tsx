29b454033af91892e09f0e0cb63eb11492a2997a
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-05-21 22:00:01 +0900

    fix questionPreview

--- components/reused/question/QuestionPreview.tsx
+++ components/reused/question/QuestionPreview.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent, useMemo } from "react";
 import styles from "../../../styles/quesiton/QuestionPreview.module.scss";
 import { QuestionPost } from "../../../api";
 import NextLink from "next/link";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
 interface Props {
   post: QuestionPost;
@@ -12,6 +14,13 @@ const summarize = (content: string) => content.substring(0, 300);
 const QuestionPreview: FunctionComponent<Props> = ({ post }) => {
   const styleBgs = [styles.bg1, styles.bg2, styles.bg3];
   const summary = useMemo(() => summarize(post.content), [post.content]);
+
+  const questionSummary = useEditor({
+    editable: false,
+    extensions: [StarterKit],
+    content: summary,
+  });
+
   return (
     <div className={styles.preview}>
       <div className={styles.previewHeader1}>
@@ -30,7 +39,7 @@ const QuestionPreview: FunctionComponent<Props> = ({ post }) => {
       <NextLink href={`/question/${post.pk}`} passHref>
         <a className={styles.previewHeader2}>{post.title}</a>
       </NextLink>
-      <div className={styles.previewBody}>{summary}</div>
+      <EditorContent editor={questionSummary} className={styles.previewBody} />
     </div>
   );
 };
