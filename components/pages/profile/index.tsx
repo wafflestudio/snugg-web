f07cd8ec4a03c77e1b606136cade49703e6051c3
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-06-25 10:34:08 +0900

    feat/ structure

--- components/pages/profile/index.tsx
+++ components/pages/profile/index.tsx
@@ -15,123 +15,13 @@ const ProfilePage: FC<props> = () => {
   return (
     <div className={styles.profile}>
       <div className={styles.profileLeft}>
-        <div className={styles.profileLeftUp}>
-          <div className={styles.profileInfo}>
-            <div className={styles.profileImg}>
-              <Image src={testImg} width={100} height={100} alt={"profile"} />
-            </div>
-            <div className={styles.profileNameNPoint}>
-              <div className={styles.profileName}>홍길동</div>
-              <div className={styles.profilePoint}>100point</div>
-            </div>
-          </div>
-          <div className={styles.profileBio}>
-            한줄 소개: 가나다라마바사아자차카타파하
-          </div>
-          <div className={styles.profileSchoolInfos}>
-            <div className={styles.schoolInfo}>
-              <div>
-                <span className={styles.schoolInfoText}>학교:</span>
-                <span>서울대학교</span>
-              </div>
-              <div className={styles.schoolInfoIcon}>
-                <Image src={EditIcon} alt={"edit"} />
-              </div>
-            </div>
-            <div className={styles.schoolInfo}>
-              <div>
-                <span className={styles.schoolInfoText}>학과:</span>
-                <span>컴퓨터공학부</span>
-              </div>
-              <div className={styles.schoolInfoIcon}>
-                <Image src={EditIcon} alt={"edit"} />
-              </div>
-            </div>
-            <div className={styles.schoolInfo}>
-              <div>
-                <span className={styles.schoolInfoText}>학년:</span>
-                <span>3학년</span>
-              </div>
-              <div className={styles.schoolInfoIcon}>
-                <Image src={EditIcon} alt={"edit"} />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.profileStat}>
-          <div className={styles.profileStatHeader}>
-            <span className={styles.profileStatHeaderText}>Statistics</span>
-            <div className={styles.profileStatHeaderButtons}>
-              <button className={styles.profileStatHeaderButton}>태그</button>
-              <button className={styles.profileStatHeaderButton}>
-                전공 분야
-              </button>
-            </div>
-          </div>
-          <div className={styles.profileStatBody}>
-            <div className={styles.Boxes}>
-              <TagStatBox />
-              <TagStatBox />
-            </div>
-            <div className={styles.Boxes}>
-              <TagStatBox />
-              <TagStatBox />
-            </div>
-          </div>
-        </div>
+        <div className={styles.basicProfile}>basicProfile</div>
+        <div className={styles.schoolInfo}>schoolInfo</div>
+        <div className={styles.accountManager}>accountManager</div>
       </div>
       <div className={styles.profileRight}>
-        <div className={styles.profileQuestions}>
-          <div className={styles.profileQuestionsHeader}>
-            <span className={styles.profileQuestionsHeaderText}>
-              내가 쓴 질문
-            </span>
-            <div className={styles.showMoreButton}>
-              <span className={styles.showMoreButtonText}>더보기</span>
-              <Image src={PlusIcon} alt={"more"} />
-            </div>
-          </div>
-          <div className={styles.profileQuestionsBody}>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-          </div>
-        </div>
-        <div className={styles.profileQuestions}>
-          <div className={styles.profileQuestionsHeader}>
-            <span className={styles.profileQuestionsHeaderText}>
-              내가 쓴 답변
-            </span>
-            <div className={styles.showMoreButton}>
-              <span className={styles.showMoreButtonText}>더보기</span>
-              <Image src={PlusIcon} alt={"more"} />
-            </div>
-          </div>
-          <div className={styles.profileQuestionsBody}>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-            <div className={styles.Previews}>
-              <SimplifiedPreview />
-              <SimplifiedPreview />
-            </div>
-          </div>
-        </div>
+        <div className={styles.statistics}>statistics</div>
+        <div className={styles.myQuestion}>myQuestion</div>
       </div>
     </div>
   );
