616ef4c99d40a7b14e8efbca6ef215daf84f004c
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-06-25 14:11:14 +0900

    feat/ basic profile

--- components/pages/profile/index.tsx
+++ components/pages/profile/index.tsx
@@ -1,21 +1,59 @@
 import React, { FC } from "react";
 import styles from "./styles.module.scss";
-import Image from "next/image";
+
 import testImg from "../../../Image/magnifier.svg";
 import EditIcon from "../../../Image/edit_icon_151377.svg";
-import TagStatBox from "../question/QuestionTagsPage/TagStatBox";
-import SimplifiedPreview from "../../reused/question/SimplifiedPreview";
-import PlusIcon from "../../../Image/plus.svg";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import PushPinIcon from "@mui/icons-material/PushPin";
+import Image from "next/image";
 
 interface props {
   id: number;
 }
 
 const ProfilePage: FC<props> = () => {
+  const introductionText = "안녕하세요 \n 가나다라마바사아자차카타파하";
+
   return (
     <div className={styles.profile}>
       <div className={styles.profileLeft}>
-        <div className={styles.basicProfile}>basicProfile</div>
+        <div className={styles.basicProfile}>
+          <div className={styles.basicProfileEdit}>
+            <button className={styles.editIcon}>
+              <Image src={EditIcon} width={25} height={25} alt={"Edit"} />
+            </button>
+          </div>
+          <div className={styles.basicProfileMain}>
+            <div className={styles.basicProfileUp}>
+              <div className={styles.profileImage}>
+                <Image
+                  src={testImg}
+                  width={"150px"}
+                  height={"150px"}
+                  alt={"Profile Img"}
+                />
+              </div>
+              <div className={styles.basicProfileRight}>
+                <div className={styles.username}>USERNAME</div>
+                <div className={styles.point}>
+                  <DiamondIcon className={styles.pointIcon} />
+                  <div>30p</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.basicProfileDown}>
+              <div className={styles.introduction}>
+                <PushPinIcon className={styles.pinIcon} />
+                <div>소개</div>
+              </div>
+              <div className={styles.introductionTextBox}>
+                <div className={styles.introductionText}>
+                  {introductionText}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className={styles.schoolInfo}>schoolInfo</div>
         <div className={styles.accountManager}>accountManager</div>
       </div>
