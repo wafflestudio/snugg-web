f2195f3e4a38b452d65d2c191f06ca7d2b98ef14
ChrisLee02 <hdchris765@snu.ac.kr>
2022-06-29 14:56:29 +0900

    "죄송합니다죄송합니다죄송합니다죄송합니다죄송합니다죄송합니다"

--- components/reused/question/AnswerBox.tsx
+++ components/reused/question/AnswerBox.tsx
@@ -11,16 +11,14 @@ import { Button, Divider, Input } from "@mui/material";
 import CommentBox from "./CommentBox";
 import { FC, useState } from "react";
 import { AnswerPostInfo } from "../../../api";
+import Moment from "react-moment";
 
 interface Props {
   onDeleteAnswer: (id: number) => void;
   AnswerData: AnswerPostInfo;
 }
 
-const AnswerBox: FC<Props> = ({
-  AnswerData: { pk },
-  onDeleteAnswer,
-}: Props) => {
+const AnswerBox: FC<Props> = ({ AnswerData, onDeleteAnswer }: Props) => {
   const [commentOpen, setCommentOpen] = useState<boolean>(false);
 
   return (
@@ -29,15 +27,17 @@ const AnswerBox: FC<Props> = ({
         <CheckIcon className={styles.questionMarkIcon} />
         <div>채택 완료</div>
       </div>
-      <div className={styles.questionText}>
-        <LoremIpsum p={2} />
-      </div>
+      <div className={styles.questionText}>{AnswerData.content}</div>
       <div className={styles.questionBottom}>
         <div className={styles.questionInfo}>
           <AccountCircleIcon className={styles.accountCircleIcon} />
-          <div className={styles.questionUser}>user 님의 답변</div>
+          <div className={styles.questionUser}>
+            {AnswerData.writer.username} 님의 답변
+          </div>
           <div className={styles.answerCount}>답변 20 채택 10</div>
-          <div className={styles.questionTime}>2022.02.28</div>
+          <div className={styles.questionTime}>
+            <Moment format={"YYYY.MM.DD"}>{AnswerData.created_at}</Moment>
+          </div>
         </div>
         <div className={styles.questionButtons}>
           <div className={styles.questionButton}>
@@ -46,7 +46,7 @@ const AnswerBox: FC<Props> = ({
           </div>
           <div
             onClick={() => {
-              onDeleteAnswer(pk);
+              onDeleteAnswer(AnswerData.pk);
             }}
             className={styles.questionButton}
           >
