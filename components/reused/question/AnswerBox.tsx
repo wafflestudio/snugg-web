a37f6af835603a7979db35e1f0cacbc4cb817410
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-07-29 14:50:22 +0900

    fix list answer comments

--- components/reused/question/AnswerBox.tsx
+++ components/reused/question/AnswerBox.tsx
@@ -85,87 +85,84 @@ const AnswerBox: FC<Props> = ({
 
   const { data: answerComments, error: answerCommentsError } =
     useQnaCommentsListQuery({ answer: answerData.pk });
-  if (answerCommentsError) {
-    return <span>error</span>;
-  } else if (!answerComments) {
-    return <span>loading...</span>;
-  } else
-    return (
-      <div className={styles.questionBox}>
-        <div className={styles.questionTitle}>
-          {accepted ? (
-            <>
-              <CheckIcon className={styles.questionMarkIcon} />
-              <div>채택 완료</div>
-            </>
-          ) : (
-            acceptable && (
-              <Button
-                className={styles.checkButton}
-                onClick={() => onAcceptAnswer()}
-              >
-                <CheckBoxOutlineBlankIcon />
-                <div>채택하기</div>
-              </Button>
-            )
-          )}
-        </div>
-        <EditorContent editor={answerView} className={styles.questionText} />
-        <div className={styles.questionBottom}>
-          <div className={styles.questionInfo}>
-            <AccountCircleIcon className={styles.accountCircleIcon} />
-            <div className={styles.questionUser}>
-              {answerData.writer?.username} 님의 답변
-            </div>
-            <div className={styles.answerCount}>답변 20 채택 10</div>
-            <div className={styles.questionTime}>
-              <Moment format={"YYYY.MM.DD"}>{answerData.created_at}</Moment>
-            </div>
+
+  return (
+    <div className={styles.questionBox}>
+      <div className={styles.questionTitle}>
+        {accepted ? (
+          <>
+            <CheckIcon className={styles.questionMarkIcon} />
+            <div>채택 완료</div>
+          </>
+        ) : (
+          acceptable && (
+            <Button
+              className={styles.checkButton}
+              onClick={() => onAcceptAnswer()}
+            >
+              <CheckBoxOutlineBlankIcon />
+              <div>채택하기</div>
+            </Button>
+          )
+        )}
+      </div>
+      <EditorContent editor={answerView} className={styles.questionText} />
+      <div className={styles.questionBottom}>
+        <div className={styles.questionInfo}>
+          <AccountCircleIcon className={styles.accountCircleIcon} />
+          <div className={styles.questionUser}>
+            {answerData.writer?.username} 님의 답변
           </div>
-          <div className={styles.questionButtons}>
-            <Button
-              className={styles.questionButton}
-              disabled={userInfo?.pk !== answerData.writer?.pk}
-            >
-              <EditIcon className={styles.questionButtonIcon} />
-              <div>수정하기</div>
-            </Button>
-            <Button
-              disabled={userInfo?.pk !== answerData.writer?.pk}
-              onClick={() => {
-                answerData.pk && onDeleteAnswer(answerData.pk);
-              }}
-              className={styles.questionButton}
-            >
-              <DeleteIcon className={styles.questionButtonIcon} />
-              <div>삭제하기</div>
-            </Button>
-            <Button
-              className={styles.questionButton}
-              onClick={() => setCommentOpen(!commentOpen)}
-            >
-              <ChatBubbleIcon className={styles.questionButtonIcon} />
-              <div>댓글쓰기</div>
-            </Button>
+          <div className={styles.answerCount}>답변 20 채택 10</div>
+          <div className={styles.questionTime}>
+            <Moment format={"YYYY.MM.DD"}>{answerData.created_at}</Moment>
           </div>
         </div>
-        <div
-          className={commentOpen ? styles.commentSection : styles.commentClosed}
-        >
-          <Divider className={styles.commentDivider} />
-          <div className={styles.commentTitle}>N개의 댓글</div>
-          <div className={styles.writeComment}>
-            <AccountCircleIcon className={styles.accountCircleIcon} />
-            <Input disableUnderline={true} placeholder="댓글을 남겨주세요." />
-            <Button>등록</Button>
-          </div>
-          {answerComments &&
-            answerComments.results!.map((item) => (
-              <CommentBox key={item.pk} commentData={item} />
-            ))}
+        <div className={styles.questionButtons}>
+          <Button
+            className={styles.questionButton}
+            disabled={userInfo?.pk !== answerData.writer?.pk}
+          >
+            <EditIcon className={styles.questionButtonIcon} />
+            <div>수정하기</div>
+          </Button>
+          <Button
+            disabled={userInfo?.pk !== answerData.writer?.pk}
+            onClick={() => {
+              answerData.pk && onDeleteAnswer(answerData.pk);
+            }}
+            className={styles.questionButton}
+          >
+            <DeleteIcon className={styles.questionButtonIcon} />
+            <div>삭제하기</div>
+          </Button>
+          <Button
+            className={styles.questionButton}
+            onClick={() => setCommentOpen(!commentOpen)}
+          >
+            <ChatBubbleIcon className={styles.questionButtonIcon} />
+            <div>댓글쓰기</div>
+          </Button>
         </div>
       </div>
-    );
+      <div
+        className={commentOpen ? styles.commentSection : styles.commentClosed}
+      >
+        <Divider className={styles.commentDivider} />
+        <div className={styles.commentTitle}>N개의 댓글</div>
+        <div className={styles.writeComment}>
+          <AccountCircleIcon className={styles.accountCircleIcon} />
+          <Input disableUnderline={true} placeholder="댓글을 남겨주세요." />
+          <Button>등록</Button>
+        </div>
+        {!answerCommentsError &&
+          answerComments &&
+          answerComments.results!.map((item) => (
+            <CommentBox key={item.pk} commentData={item} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default AnswerBox;
