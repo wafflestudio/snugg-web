ee75e9ef3cf9ba757b955b3be38855e8054db3fd
joongwon <winterdawnlight@snu.ac.kr>
2022-07-29 18:48:50 +0900

    add proper summary

--- components/reused/question/QuestionPreview.tsx
+++ components/reused/question/QuestionPreview.tsx
@@ -1,10 +1,10 @@
 import React, { FunctionComponent, useMemo } from "react";
 import styles from "../../../styles/quesiton/QuestionPreview.module.scss";
 import NextLink from "next/link";
-import { EditorContent, JSONContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import { generateHTML, JSONContent } from "@tiptap/react";
 import { Post } from "../../../store/api/injected";
 import { forceType } from "../../../utility";
+import { editorExtensions } from "../QuestionEditor";
 
 interface Props {
   post: Post;
@@ -14,22 +14,16 @@ const summarize = (content: string) => content.substring(0, 300);
 
 const QuestionPreview: FunctionComponent<Props> = ({ post }) => {
   const styleBgs = [styles.bg1, styles.bg2, styles.bg3];
-  const summary = useMemo(() => summarize(post.content), [post.content]);
-
-  let jsonContent: JSONContent | undefined;
-  let success: boolean;
-  try {
-    jsonContent = JSON.parse(post.content);
-    success = true;
-  } catch (err) {
-    success = false;
-  }
-
-  const questionSummary = useEditor({
-    editable: false,
-    extensions: [StarterKit],
-    content: success ? jsonContent : summary, // TODO jsonContent is not summarized
-  });
+  const summary = useMemo(() => {
+    try {
+      const content = JSON.parse(post.content) as JSONContent;
+      const html = generateHTML(content, editorExtensions);
+      const dom = new DOMParser().parseFromString(html, "text/html");
+      return summarize(dom.documentElement.innerText);
+    } catch (e) {
+      return summarize(post.content);
+    }
+  }, [post.content]);
 
   const tags = forceType<string[]>(post.tags);
 
@@ -51,7 +45,7 @@ const QuestionPreview: FunctionComponent<Props> = ({ post }) => {
       <NextLink href={`/question/${post.pk}`} passHref>
         <a className={styles.previewHeader2}>{post.title}</a>
       </NextLink>
-      <EditorContent editor={questionSummary} className={styles.previewBody} />
+      <div className={styles.previewBody}>{summary}</div>
     </div>
   );
 };
